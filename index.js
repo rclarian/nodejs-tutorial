ec640d3805744875e40e9486127937d98e1e7c8c
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-27 21:04:39 +0800

    13 - Module Caching
    13 - Module Caching

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-require('./batman')
-require('./superman')
+// const superHero = require('./super-hero');
+// console.log(superHero);
+
+// superHero.setName('Superman');
+// console.log(superHero.getName());
+
+// const newSuperHero = require('./super-hero');
+// console.log(newSuperHero.getName());
+
+const SuperHero = require("./super-hero");
+
+const batman = new SuperHero('Batman');
+console.log(batman.getName());
+batman.setName("Bruce Wayne");
+console.log(batman.getName());
+
+const superman = new SuperHero("Superman");
+console.log(superman.getName());
+
