206f59b73fd17ebb32cf072fb185ac8e8765d162
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-03-28 11:55:23 +0800

    17 - Importing JSON and Watch Mode - node  --watch index
    17 - Importing JSON and Watch Mode - node  --watch index

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// const add = require("./math-esm.mjs");
-// console.log(add(2, 5));
+const data = require("./data.json");
 
-const math = require("./math-esm.mjs");
-
-// console.log(math.add(2, 5));
-// console.log(math.subtract(50, 22));
-// console.log(math.multiply(10, 2));
-
-const { add, subtract } = math;
-
-console.log(add(2, 5));
-console.log(subtract(50, 22));
+console.log(data.name);
