38fe1dd7ccff128c0d34333f2489c11db1e3f640
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-04-09 21:00:02 +0800

    26 - fs Module - fs.readFileSync, fs.writeFileSync, fs.writeFile
    26 - fs Module - fs.readFileSync, fs.writeFileSync, fs.writeFile

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const { error } = require("node:console");
+const fs = require("node:fs");
 
-const buffer = new Buffer.from("Vishwas");
+console.log("First");
 
-buffer.write("Code");
-console.log(buffer.toString());
-console.log(buffer);
-console.log(buffer.toJSON());
+const fileContents = fs.readFileSync("./file.txt", "utf-8");
+console.log(fileContents);
+
+console.log("Second");
+fs.readFile("./file.txt", "utf-8", (error, data) => {
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(data);
+    }
+});
+console.log("3rd");
+
+fs.writeFileSync("./greet.txt", "Hello world ryan!");
+fs.writeFile("./greet.txt", " Hello world ryan1!", { flag: "a" }, (error) => {
+    if(error){
+        console.log(error);
+    }else{
+        console.log("File written");
+    }
+});
