3d5503216ff0240e615e75bf5e4ea54823821d8a
Ryan Clarian <rclarian@gmail.com>
2024-04-11 12:51:01 +0800

    27 - fs Promise Module - node:fs/promises
    27 - fs Promise Module - node:fs/promises

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const { error } = require("node:console");
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
 console.log("First");
 
-const fileContents = fs.readFileSync("./file.txt", "utf-8");
-console.log(fileContents);
+async function readFile() {
+    try{
+        const data = await fs.readFile("file.txt", "utf-8");
+        console.log(data);
+    }catch(err){
+        console.log(err);
+    }
+}
 
 console.log("Second");
-fs.readFile("./file.txt", "utf-8", (error, data) => {
-    if(error) {
-        console.log(error);
-    }
-    else{
-        console.log(data);
-    }
-});
-console.log("3rd");
 
-fs.writeFileSync("./greet.txt", "Hello world ryan!");
-fs.writeFile("./greet.txt", " Hello world ryan1!", { flag: "a" }, (error) => {
-    if(error){
-        console.log(error);
-    }else{
-        console.log("File written");
-    }
-});
+readFile();
+
+// console.log("First");
+
+// fs.readFile("file.txt", "utf-8")
+// .then((data) => console.log(data))
+// .catch((error) => console.log(error));
+
+// console.log("Second");
+
+// const fs = require("node:fs");
+
+// console.log("First");
+
+// const fileContents = fs.readFileSync("./file.txt", "utf-8");
+// console.log(fileContents);
+
+// console.log("Second");
+// fs.readFile("./file.txt", "utf-8", (error, data) => {
+//     if(error) {
+//         console.log(error);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// });
+// console.log("3rd");
+
+// fs.writeFileSync("./greet.txt", "Hello world ryan!");
+// fs.writeFile("./greet.txt", " Hello world ryan1!", { flag: "a" }, (error) => {
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log("File written");
+//     }
+// });
