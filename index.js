aca2b9abe3d9e077e58488c4230fc13a63573d6f
Ryan Clarian <rclarian@gmail.com>
2024-04-12 10:55:02 +0800

    34 - HTML Template
    34 - HTML Template

--- index.js
+++ index.js
@@ -2,22 +2,13 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-    const superHero = {
-        firstName: "Bruce",
-        lastName: "Wayne",
-    };
-
+    
+    const name = "Vishwas";
     res.writeHead(200, {"Content-Type": "text/html"});
 
-    //3rd option
-    fs.createReadStream(__dirname + "/index.html").pipe(res);
-
-    //2nd option
-    //fs.createReadStream("./index.html").pipe(res);
-
-    //1st option
-    // const html = fs.readFileSync("./index.html", "utf-8");
-    // res.end(html);
+    let html = fs.readFileSync("./index.html", "utf-8");
+    html = html.replace("{{name}}", name);
+    res.end(html);
 });
 
 server.listen(3000, () => {
