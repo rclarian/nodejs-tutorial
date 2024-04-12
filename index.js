d164410ac6b9838ff2a7ab2840a90bf131c7306e
Ryan Clarian <rclarian@gmail.com>
2024-04-12 12:46:22 +0800

    35 - HTTP Routing - req.url === "/", req.url === "/about"
    35 - HTTP Routing - req.url === "/", req.url === "/about"

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
+    if(req.url === "/"){
+        res.writeHead(200, { "Content-Type": "text/plain"});
+        res.end("Home page");
+    }
+    else if(req.url === "/about"){
+        res.writeHead(200, { "Content-Type": "text/plain"});
+        res.end("About page");
+    }
+    else if(req.url === "/api"){
+        res.writeHead(200, { "Content-Type": "application/json"});
+        res.end(JSON.stringify({
+            firstName: "Bruce",
+            lastName: "Wayne",
+        }));
+    }
+    else {
+        res.writeHead(404);
+        res.end("Page not found");
+    }
     
-    const name = "Vishwas";
-    res.writeHead(200, {"Content-Type": "text/html"});
-
-    let html = fs.readFileSync("./index.html", "utf-8");
-    html = html.replace("{{name}}", name);
-    res.end(html);
 });
 
 server.listen(3000, () => {
