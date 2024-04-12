da5c7acbbb142f2a5187c3826ddf31546118196f
Ryan Clarian <rclarian@gmail.com>
2024-04-12 15:28:02 +0800

    41 - Network I/O - https.request
    41 - Network I/O - https.request

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const crypto = require("node:crypto");
+//Example 6
+const https = require("node:https");
 
-process.env.UV_THREADPOOL_SIZE = 6; //number of threads
-const MAX_CALLS = 6;
+const MAX_CALLS = 12;
 const start = Date.now();
 
 for(let i = 0; i < MAX_CALLS; i++ ) {
-    crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
-        console.log(`Hash: ${i + 1}`, Date.now() - start);
-    });
+    https.request("https://google.com", (res) => {
+        res.on("data", () => {});
+        res.on("end", () => {
+            console.log(`Reques: ${i + 1}`, Date.now() - start);
+        });
+    }).end();
 }
 
+//Example 5
+// const crypto = require("node:crypto");
+
+// process.env.UV_THREADPOOL_SIZE = 6; //number of threads
+// const MAX_CALLS = 6;
+// const start = Date.now();
+
+// for(let i = 0; i < MAX_CALLS; i++ ) {
+//     crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () => {
+//         console.log(`Hash: ${i + 1}`, Date.now() - start);
+//     });
+// }
+
+//Example 4
 // const start = Date.now();
 // crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
 // crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
