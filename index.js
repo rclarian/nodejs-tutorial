0d9735367f84d20c46f6ec750ca90d93248f46af
Ryan Clarian <rclarian@gmail.com>
2024-04-17 13:55:44 +0800

    48 - Close Queue
    48 - Close Queue

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-//13th Experiment
-setTimeout(() => console.log("this is inner setTimeout 1"), 0);
+//14th Experiment
+const fs = require("node:fs");
+
+const readableStream = fs.createReadStream(__filename);
+readableStream.close();
+
+readableStream.on("close", () => {
+    console.log("this is from readableStream close event callback");
+});
+
 setImmediate(() => console.log("this is inner setImmediate 1"));
+setTimeout(() => console.log("this is inner setTimeout 1"), 0);
+Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+process.nextTick(() => console.log("this is process.nextTick 1"));
+
+
+//13th Experiment
+//setTimeout(() => console.log("this is inner setTimeout 1"), 0);
+//setImmediate(() => console.log("this is inner setImmediate 1"));
 
 //12th Experiment
 // setImmediate(() => console.log("this is inner setImmediate 1"));
