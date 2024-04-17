43790b839c739cf96e4cf62c2df9daf778943b58
Ryan Clarian <rclarian@gmail.com>
2024-04-17 13:45:02 +0800

    47 - Check Queue
    47 - Check Queue

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-//9th Experiment
-const fs = require("node:fs");
+//13th Experiment
+setTimeout(() => console.log("this is inner setTimeout 1"), 0);
+setImmediate(() => console.log("this is inner setImmediate 1"));
 
-fs.readFile(__filename, () => {
-    console.log("this is readFile 1");
-});
+//12th Experiment
+// setImmediate(() => console.log("this is inner setImmediate 1"));
+// setImmediate(() => {
+//         console.log("this is inner setImmediate 2");
+//         process.nextTick(() => console.log("this is process.nextTick 1"));
+//         Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+//     });
+// setImmediate(() => console.log("this is inner setImmediate 3"));
 
-process.nextTick(() => console.log("this is process.nextTick 1"));
-Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
-setTimeout(() => console.log("this is setTimeout 1"), 0);
-setImmediate(() => console.log("this is setImmediate 1"));
+//11th Experiment
+// const fs = require("node:fs");
 
-for (let i = 0; i < 20000000000; i++){
+// fs.readFile(__filename, () => {
+//     console.log("this is readFile 1");
+//     setImmediate(() => console.log("this is inner setImmediate inside readFile"));
+//     process.nextTick(() => 
+//         console.log("this is the inner process.nextTick inside readFile")
+//     );
+//     Promise.resolve().then(() => 
+//         console.log("this is the inner Promise.resolve inside readFile")
+//     );
+// });
 
-}
+// process.nextTick(() => console.log("this is process.nextTick 1"));
+// Promise.resolve().then(() => console.log("this is Promise.resolve 1"));
+// setTimeout(() => console.log("this is setTimeout 1"), 0);
+
+
+// for (let i = 0; i < 20000000000; i++){
+
+// }
