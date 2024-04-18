71c8378ecb78bdc99745dbddb979a731d2f77830
Ryan Clarian <rclarian@gmail.com>
2024-04-18 15:26:59 +0800

    59 - CLI Options
    59 - CLI Options

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log("codevolution pokedex");
+//console.log("codevolution pokedex");
+
+const printFiveMoves = async (pokemonName) => {
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const pokemon = await response.json();
+    const moves = pokemon.moves.map(({ move }) => move.name);
+    console.log(moves.slice(0, 5));
+};
+
+printFiveMoves("charmander");
